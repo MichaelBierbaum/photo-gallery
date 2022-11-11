d6a10acee9e7cb3b4f9d059784c84a90d57cfd88
maestro979546 <maestro979546@web.de>
2022-11-11 21:30:00 +0100

    build and debug android app

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { PhotoService } from '../services/photo.service';
-
+import { PhotoService, UserPhoto } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
 
 
 
@@ -12,7 +12,8 @@ import { PhotoService } from '../services/photo.service';
 })
 export class Tab2Page {
 
-  constructor(public photoService: PhotoService) { }
+  constructor(public photoService: PhotoService,
+              public actionSheetController: ActionSheetController) { }
 
   //when the user first navigates to Tab 2 (the Photo Gallery), 
   
@@ -24,6 +25,28 @@ export class Tab2Page {
   //and displayed on the screen
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
+  }
+
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
   }
 
 }
